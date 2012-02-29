8772c49774d0a0333471ab80da670f93e03a15d3
Michael Flowers <mikesflowers@gmail.com>
2012-02-29 02:05:33 +0100

    General: Added all files for Netbeans project General: Added LADJava file for first Java file

---
+++ LAD-Java/src/lad/java/LADJava.java
@@ -0,0 +1,15 @@
+package lad.java;
+
+/**
+ *
+ * @author msflowers
+ */
+public class LADJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
