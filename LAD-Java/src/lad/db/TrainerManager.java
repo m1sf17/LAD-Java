3ab2ee8590bfd9d82091279af21b18442d5a0d85
m1sf17 <mikesflowers@gmail.com>
2012-04-04 18:51:20 +0300

    Online Edit: Trainers: Migrated table validation to remote calls

--- LAD-Java/src/lad/db/TrainerManager.java
+++ LAD-Java/src/lad/db/TrainerManager.java
@@ -84,120 +84,51 @@ public class TrainerManager
      */
     private void validateTablesExist()
     {
-        String[] loadedTables = MySQLDB.getTableList();
-        boolean foundMinion = false;
-        boolean foundTrainer = false;
-        Connection conn = MySQLDB.getConn();
-
-        // Check the list of tables to see if ours are in there
-        for( int i = 0; i < loadedTables.length; i++ )
-        {
-            String tbl = loadedTables[ i ];
-            if( tbl.compareToIgnoreCase( "MINIONS" ) == 0 )
-            {
-                foundMinion = true;
-            }
-            else if( tbl.compareToIgnoreCase( "TRAINERS" ) == 0 )
-            {
-                foundTrainer = true;
-            }
-        }
-
-        if( !foundMinion )
-        {
-            try
-            {
-                Statement stmt = conn.createStatement();
-                stmt.executeUpdate( minionCreationStr );
-            }
-            catch( SQLException e )
-            {
-                System.err.println( "Error while creating minion table: " +
-                                    e.toString() );
-            }
-        }
-        if( !foundTrainer )
-        {
-            try
-            {
-                Statement stmt = conn.createStatement();
-                stmt.executeUpdate( trainerCreationStr );
-            }
-            catch( SQLException e )
-            {
-                System.err.println( "Error while creating trainer table: " +
-                                    e.toString() );
-            }
-        }
-
-        // If one wasn't found, update the list of tables
-        if( !foundMinion || !foundTrainer )
-        {
-            MySQLDB.getInstance().populateTableList();
-        }
-
-        // If one was found, check both integrity
-        if( foundMinion || foundTrainer )
-        {
-            validateStructures();
-        }
-    }
-
-    /**
-     * Validates that all of the MySQL Tables are valid for information.
-     */
-    private void validateStructures()
-    {
-        String[] trainerFields = { "ID", "owner", "exp", "level" };
-        String[] minionFields = { "ID", "owner", "exp", "level" };
-
-        // Grab actual data and populate list
-        Connection conn = MySQLDB.getConn();
+        MySQLDB db = MySQLDB.getInstance();
         try
         {
-            boolean trainerValid = true;
-            boolean minionValid = true;
-            Statement stmt = conn.createStatement();
-            ResultSet result = stmt.executeQuery( "SHOW COLUMNS FROM " +
-                                                  "TRAINERS" );
-            int i = 0;
-            while( result.next() )
-            {
-                if( trainerFields[ i ].compareToIgnoreCase( result.getString( 1 ) ) != 0 )
-                {
-                    throw new SQLException( "Trainer table error." );
-                }
-                i++;
-            }
-            if( i != trainerFields.length )
-            {
-                throw new SQLException( "Trainer table column miscount." );
-            }
-
-            stmt = conn.createStatement();
-            result = stmt.executeQuery( "SHOW COLUMNS FROM MINIONS" );
-            i = 0;
-            while( result.next() )
-            {
-                if( minionFields[ i ].compareToIgnoreCase( result.getString( 1 ) ) != 0 )
-                {
-                    throw new SQLException( "Minion table error." );
-                }
-                i++;
-            }
-            if( i != minionFields.length )
-            {
-                throw new SQLException( "Minion table column miscount." );
-            }
-
+            db.validateTable( "TRAINERS", trainerCreationStr );
         }
         catch( SQLException e )
         {
-            e.printStackTrace();
-            System.err.println( "Error while validating trainers and minions:" +
+            System.err.println( "Error with trainer table." +
                                 e.toString() );
             System.exit( -1 );
         }
+        try
+        {
+            db.validateStructure( { "ID", "owner", "exp", "level" },
+                                  "TRAINERS" );
+        }
+        catch( SQLException e )
+        {
+            System.err.println( "Error with trainer headers." +
+                                e.toString() );
+            System.exit( -1 );
+        }
+        try
+        {
+            db.validateTable( "MINIONS", minionCreationStr );
+        }
+        catch( SQLException e )
+        {
+            System.err.println( "Error with minion table." +
+                                e.toString() );
+            System.exit( -1 );
+        }
+        try
+        {
+            db.validateStructure( { "ID", "owner", "exp", "level" },
+                                  "MINIONS" );
+        }
+        catch( SQLException e )
+        {
+            System.err.println( "Error with minion headers." +
+                                e.toString() );
+            System.exit( -1 );
+        }
+
+
     }
 
     /**
