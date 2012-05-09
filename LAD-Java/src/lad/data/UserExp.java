52dedb10e7456fb18a6cdb4669abc90cf23f80fb
Michael Flowers <mikesflowers@gmail.com>
2012-05-09 16:30:22 +0200

    UserExp: Unused import

--- LAD-Java/src/lad/data/UserExp.java
+++ LAD-Java/src/lad/data/UserExp.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import lad.db.EXPManager;
 import lad.db.MySQLDB;
 import lad.db.TableProfile;
